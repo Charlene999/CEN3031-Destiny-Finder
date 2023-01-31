ea46d9d566a0688c9e10f582775280d832b7cea1
kerryhannigan <kerrysue.hannigan@gmail.com>
2023-01-31 16:26:02 -0500

    Hash password, create jwt for logging in

--- application/server/controllers/user.go
+++ application/server/controllers/user.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"fmt"
+	"os"
+
 	"backend/database"
 	"backend/models"
 	"errors"
@@ -8,6 +11,10 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+
+	"golang.org/x/crypto/bcrypt"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/joho/godotenv"
 )
 
 type UserRepo struct {
@@ -28,7 +35,14 @@ func (repository *UserRepo) CreateUser(c *gin.Context) {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err})
 		return
 	}
-	user := models.User{Name: buildUser.Name, Username: buildUser.Username, Email: buildUser.Email, Password: buildUser.Password, IsAdmin: false}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(buildUser.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusBadGateway, gin.H{"status": "error", "message": "Could not hash password"})
+		return
+	}
+
+	user := models.User{Name: buildUser.Name, Username: buildUser.Username, Email: buildUser.Email, Password: string(hashedPassword), IsAdmin: false}
 
 	//Check to make sure there is no other user with the same username
 	var existingUser models.User
@@ -62,4 +76,55 @@ func (repository *UserRepo) GetUser(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, user)
+}
+
+// I tried to put this in a new shared Utils package but the func was "undefined" ?
+func goDotEnvVariable(key string) string {
+	err := godotenv.Load()
+
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+// TODO: (KH) break apart into separate methods to be called by LogIn
+// i.e. method to CheckForExistingUser, method to CheckPassword, GenerateToken, etc. 
+// maybe put in shared Utils or Helpers package? 
+func (repository *UserRepo) LogIn(c *gin.Context) {
+	var signInInput models.UserSignIn
+	err := c.ShouldBindJSON(&signInInput)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+
+	var user models.User
+	err = repository.Db.First(&user, "username = ?", signInInput.Username).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": signInInput.Username})
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(signInInput.Password))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadGateway, gin.H{"error": signInInput.Password})
+		return
+	}
+
+	secret := goDotEnvVariable("TOKEN_SECRET")
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"Username": signInInput.Username,
+	})
+	tokenString, err := token.SignedString([]byte(secret))
+
+	fmt.Printf("%v", err)
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Could not generate token"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token":tokenString})
 }
