9e8cd651f3504c614e9dcb48c7a04559482c9899
Charlene Creighton <creightonc@ufl.edu>
2023-02-02 22:26:25 -0500

    Login form fields changed with CSS, when login pressed

--- application/src/app/app.routing.ts
+++ application/src/app/app.routing.ts
@@ -2,8 +2,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './loginPageElements/login-page';
 
 const routes: Routes = [
-  { path: 'login', component: LoginPageComponent },
-  { path: '**', redirectTo: '' }
+  { path: 'login', component: LoginPageComponent }
+  //{ path: '**', redirectTo: '' }
 ];
 
 export const appRoutingModule = RouterModule.forRoot(routes);
