f2e38105755b8d51e11fbfc8ef885bff7621d7c6
Charlene Creighton <creightonc@ufl.edu>
2023-02-03 04:12:49 -0500

    Sign up form created

--- application/src/app/app.routing.ts
+++ application/src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './loginPageElements/login-page';
+import { SignupComponent } from './signup';
 
 const routes: Routes = [
-  { path: 'login', component: LoginPageComponent }
-  //{ path: '**', redirectTo: '' }
+  { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '**', redirectTo: '' }
 ];
 
 export const appRoutingModule = RouterModule.forRoot(routes);
