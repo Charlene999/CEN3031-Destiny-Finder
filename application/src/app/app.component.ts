ab31f23e3e5225609468f1c9ab3dc7e36ca711e5
Dylan Tosh <dylantosh@ufl.edu>
2023-02-03 17:02:15 -0600

    Connected frontend user login and create user pages to backend and added comments

--- application/src/app/app.component.ts
+++ application/src/app/app.component.ts
@@ -1,10 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
-
-import { environment } from '../../environment';
 
 @Component({
   selector: 'app-root',
@@ -12,16 +6,4 @@ import { environment } from '../../environment';
   styleUrls: ['./app.component.css']
 })
 
-//Made changes here to test a connection between the frontend and backend; derived from the boilerplate project setup given in the class setup announcement
-export class AppComponent {
-  title = 'frontend';
-  connection = 'Backend connection not established'
-  
-  constructor(private http: HttpClient) {
-	this.http.get(`${environment.serverUrl}/ping`).subscribe((data) => {
-		var entries = Object.entries(data);
-		console.log(entries);
-		this.connection = `Backend connection established; ${entries[0][0]}: ${entries[0][1]}`;
-	})
-  }
-}
+export class AppComponent { }
--- application/src/app/loginPageElements/login-page/login-page.component.ts
+++ application/src/app/loginPageElements/login-page/login-page.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
+
 export class LoginPageComponent {
+
+  constructor(private http:HttpClient){ }
+
+  onSubmit(f: NgForm) {
+    const options = {headers: {'Content-Type': 'application/json'}};
+    this.http.post('http://localhost:8080/users/login', JSON.stringify(f.value), options).subscribe(
+      (res: any) => {
+        console.log(res);
+    });
+  }
 
 }
