ea85ece299b1cd5ab3b6527afc7a78caf68385c9
Dylan Tosh <dylantosh@ufl.edu>
2023-03-18 19:03:46 -0500

    Added home page component/Redesigned home page

--- application/src/app/app-routing.module.ts
+++ application/src/app/app-routing.module.ts
@@ -15,10 +15,13 @@ import { SignupComponent } from './signup/signup.component';
 import { PasswordComponent } from './users/users-edit/password/password.component';
 import { NameComponent } from './users/users-edit/name/name.component';
 import { EmailComponent } from './users/users-edit/email/email.component';
+import { HomeComponent } from './home/home.component';
 
 //Routing for the Entire Application
 //NOTE: path must match the routerLink indicated in app.component.html
 const routes: Routes = [
+  { path: '', component: HomeComponent },
+
   //If user not logged in, display login and create user tabs only
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
