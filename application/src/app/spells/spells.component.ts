85b76cc2c39e503edf22673be2472288b052a653
Dylan Tosh <dylantosh@ufl.edu>
2023-03-18 21:56:21 -0500

    Added ability to add and remove spells and items from characters

--- application/src/app/spells/spells.component.ts
+++ application/src/app/spells/spells.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   templateUrl: './spells.component.html',
@@ -13,12 +13,16 @@ export class SpellsComponent {
   curChar: character;
   allSpells: Spell[];
   viewSubmitted: Boolean;
+  addSubmitted: Boolean;
+  removeSubmitted: Boolean;
 
   constructor(private http: HttpClient, private router: Router) {
     this.allChars = [];
     this.allSpells = [];
     this.curChar = {} as character;
     this.viewSubmitted = false;
+    this.addSubmitted = false;
+    this.removeSubmitted = false;
   }
 
   ngOnInit() {
@@ -30,31 +34,28 @@ export class SpellsComponent {
       "OwnerToken": localStorage.getItem('id_token'),
     };
 
-    this.viewSubmitted = true;
-
     const options = { headers: { 'Content-Type': 'application/json' } };
-
     // Get all user characters
     this.http.post('http://localhost:8080/characters/get', JSON.stringify(Character), options).subscribe(data => {
       if (200) {
+
         if (data === null)
           return;
+
         var chars = JSON.parse(JSON.stringify(data));
         this.allChars.splice(0);
+
         for (var i = 0; i < chars.length; i++) {
-          var char = new character(chars[i].Name, chars[i].Level, chars[i].ClassType, chars[i].Description, chars[i].CharacterID, chars[i].Items);
+          var char = new character(chars[i].Name, chars[i].Level, chars[i].ClassType, chars[i].Description, chars[i].ID, chars[i].Spells);
           this.allChars.push(char);
         }
+
       }
     }, (error) => {
       if (error.status === 404) {
         alert('Resource not found.');
       }
-      else if (error.status === 409) {
-        alert('Character already exists. Please try another one.');
-      }
       else if (error.status === 500) {
-
         alert('Server down.');
       }
       else if (error.status === 502) {
@@ -67,6 +68,8 @@ export class SpellsComponent {
   // show all spells owned and unowned for that class and level
   showSpells() {
 
+    this.viewSubmitted = true;
+
     const select = document.getElementById("chars") as HTMLSelectElement;
     const index = select.selectedIndex;
 
@@ -76,47 +79,29 @@ export class SpellsComponent {
 
     // Current character equals user's selected option'
     var char = this.allChars.at(index - 1)!;
-    // Get all items
-    let Spells = {
-      "AdminToken": localStorage.getItem('id_token'),
-    };
-
-    this.viewSubmitted = true;
 
     const options = { headers: { 'Content-Type': 'application/json' } };
-
-    this.http.post('http://localhost:8080/spells/get', JSON.stringify(Spells), options).subscribe(data => {
+    this.http.post('http://localhost:8080/spells/get', options).subscribe(data => {
       if (200) {
+
+        if (data === null)
+          return;
+
         var spells = JSON.parse(JSON.stringify(data));
         this.allSpells.splice(0);
 
-        // Filter spells with equal class and level with character
-
         for (var i = 0; i < spells.length; i++) {
-
-          if (spells[i].LevelReq === char.Level && spells[i].ClassReq === char.Class) {
-
-            var spell = new Spell(spells[i].Name, spells[i].Description, spells[i].LevelReq, spells[i].ClassReq, spells[i].ItemID);
-
-            if (char.spells.get(spell.ID)) {
-              spell.Owned = true;
-              // If spell belongs to character, Owned should equal true
-              char.spells.get(spell.ID)!.Owned = true;
-            }
+          if (spells[i].ClassReq === char.Class) {
+            var spell = new Spell(spells[i].Name, spells[i].Description, spells[i].LevelReq, spells[i].ClassReq, spells[i].ID);
             this.allSpells.push(spell);
           }
-
         }
       }
     }, (error) => {
       if (error.status === 404) {
         alert('Resource not found.');
       }
-      else if (error.status === 409) {
-        alert('Item already exists. Please try another one.');
-      }
       else if (error.status === 500) {
-
         alert('Server down.');
       }
       else if (error.status === 502) {
@@ -126,21 +111,81 @@ export class SpellsComponent {
     );
   }
 
-  // These methods allow items to be shown as owned or unowned by a character
-  spellOwned(spell: Spell): boolean {
-    return spell.Owned;
-  }
-  spellUnowned(spell: Spell): boolean {
-    if (spell.Owned === false)
+  spellOwned(spellID: number): boolean {
+    if (this.curChar.spells.get(spellID))
       return true;
-
     else
       return false;
   }
-  // Placeholder for Adding spell to character
-  add()
-  {
-    alert("Spell added");
+
+  setCharacter(char: character) {
+    this.curChar = char;
+  }
+
+  //Add Item To Character
+  add(spellId: number) {
+
+    this.addSubmitted = true;
+
+    let addSpell = {
+      "SpellID": spellId,
+      "OwnerToken": localStorage.getItem('id_token'),
+      "CharacterID": this.curChar.ID,
+    }
+
+    const options = { headers: { 'Content-Type': 'application/json' } };
+    this.http.post('http://localhost:8080/characters/addspell', JSON.stringify(addSpell), options).subscribe(data => {
+      if (202) {
+          alert("Spell added.");
+          window.location.reload();
+        }
+      }, 
+      (error) => {
+      if (error.status === 404) {
+        alert('Resource not found.');
+      }
+      else if (error.status === 500) {
+        alert('Server down.');
+      }
+      else if (error.status === 502) {
+        alert('Bad gateway.');
+      }
+    }
+    );
+  }
+
+  //Add Item To Character
+  remove(spellID: number) {
+
+    this.removeSubmitted = true;
+
+    const options = { 
+      headers: { 'Content-Type': 'application/json' },
+      body: {       
+        "SpellID": spellID,
+        "OwnerToken": localStorage.getItem('id_token'),
+        "CharacterID": this.curChar.ID, 
+      }
+    };
+    
+    this.http.delete('http://localhost:8080/characters/removespell', options).subscribe(data => {
+      if (202) {
+          alert("Spell removed.");
+          window.location.reload();
+        }
+      }, 
+      (error) => {
+      if (error.status === 404) {
+        alert('Resource not found.');
+      }
+      else if (error.status === 500) {
+        alert('Server down.');
+      }
+      else if (error.status === 502) {
+        alert('Bad gateway.');
+      }
+    }
+    );
   }
 }
 
@@ -153,8 +198,8 @@ class character {
   Description: string;
   ID: number;
   spells: Map<number, Spell>;
-  constructor(name: string, level: number, myclass: number, desc: string, id: number, allspells: Spell[]) {
 
+  constructor(name: string, level: number, myclass: number, desc: string, id: number, allspells: Spell[]) {
     this.Name = name;
     this.Level = level;
     this.Class = myclass;
@@ -162,12 +207,12 @@ class character {
     this.ID = id;
     this.spells = new Map<number, Spell>;
 
-    if (allspells !== null)
+    if (allspells !== null) {
       for (var i = 0; i < allspells.length; i++) {
         this.spells.set(allspells[i].ID, allspells[i]);
       }
+    }
   }
-
 }
 
 class Spell {
@@ -186,4 +231,3 @@ class Spell {
     this.ID = id;
   }
 }
-
